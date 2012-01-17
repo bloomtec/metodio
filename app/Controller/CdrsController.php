3652702e9e24ddc1f562092e8977050927505a82
Julio César Domínguez Giraldo <jucedogi@ubuntu-vm.(none)>
2012-01-17 12:32:55 -0500

    revision reportes

--- app/Controller/CdrsController.php
+++ app/Controller/CdrsController.php
@@ -32,6 +32,7 @@ class CdrsController extends AppController {
 		if(isset($this->data) && !empty($this->data)) {
 			$this -> recursive = -1;
 			$conditions = array();
+			$redirect = "reporte";
 			if (isset($this -> data['Cdr']) && !empty($this -> data['Cdr'])) {
 				$data = $this -> data['Cdr'];
 	
@@ -54,12 +55,15 @@ class CdrsController extends AppController {
 					$hora_inicial['segundo'] = $data['hora_inicial']['segundo']['min'];
 					$hora_inicial['usar'] = true;
 				}
-				if ($hora_inicial['usar']) {
-					if (isset($conditions['Cdr.calldate >='])) {
+				if (isset($conditions['Cdr.calldate >='])) {
+					if ($hora_inicial['usar']) {
 						$conditions['Cdr.calldate >='] .= ' ' . $hora_inicial['hora'] . ':' . $hora_inicial['minuto'] . ':' . $hora_inicial['segundo'];
 					} else {
-						$conditions['Cdr.calldate >='] = $hora_inicial['hora'] . ':' . $hora_inicial['minuto'] . ':' . $hora_inicial['segundo'];
+						$conditions['Cdr.calldate >='] .= ' 00:00:00';
 					}
+				}
+				if(isset($conditions['Cdr.calldate >=']) && !empty($conditions['Cdr.calldate >='])) {
+					$redirect .= "/fi:" . urlencode($conditions['Cdr.calldate >=']);
 				}
 	
 				// Fecha final
@@ -80,43 +84,110 @@ class CdrsController extends AppController {
 					$hora_final['segundo'] = $data['hora_final']['segundo']['min'];
 					$hora_final['usar'] = true;
 				}
-				if ($hora_final['usar']) {
-					if (isset($conditions['Cdr.calldate <='])) {
+				if (isset($conditions['Cdr.calldate <='])) {
+					if($hora_final['usar']) {
 						$conditions['Cdr.calldate <='] .= ' ' . $hora_final['hora'] . ':' . $hora_final['minuto'] . ':' . $hora_final['segundo'];
 					} else {
-						$conditions['Cdr.calldate <='] = $hora_final['hora'] . ':' . $hora_final['minuto'] . ':' . $hora_final['segundo'];
+						$conditions['Cdr.calldate <='] .= ' 23:59:59';
 					}
+				}
+				if(isset($conditions['Cdr.calldate <=']) && !empty($conditions['Cdr.calldate <='])) {
+					$redirect .= "/ff:" . urlencode($conditions['Cdr.calldate <=']);
 				}
 	
 				// Extensión
 				if (isset($data['extension']) && !empty($data['extension'])) {
-					$conditions['Cdr.src'] = $data['extension'];
+					//$conditions['Cdr.src'] = $data['extension'];
+					$redirect .= "/exten:" . urlencode($data['extension']);
 				}
+
+				// Origen
 				if (isset($data['origen']) && !empty($data['origen'])) {
-					$conditions['Cdr.src'] = $data['origen'];
+					//$conditions['Cdr.src'] = $data['origen'];
+					$redirect .= "/src:" . urlencode($data['origen']);
 				}
+				
+				// Destino
 				if (isset($data['destino']) && !empty($data['destino'])) {
-					$conditions['Cdr.dst'] = $data['destino'];
+					//$conditions['Cdr.dst'] = $data['destino'];
+					$redirect .= "/dst:" . urlencode($data['destino']);
 				}
+
+				// Departamento
 				if (isset($data['departamento']) && !empty($data['departamento'])) {
 					$this -> loadModel('SipDispositivo');
 					$extensions = $this -> SipDispositivo -> find('list', array('conditions' => array('SipDispositivo.department_id' => $data['departamento']), 'fields' => array('SipDispositivo.name')));
-					$conditions['Cdr.src'] = $extensions;
+					//$conditions['Cdr.src'] = $extensions;
+					$tmp_extensions = "";
+					foreach($extensions as $exten) {
+						$tmp_extensions .= "$exten;";
+					}
+					$redirect .= "/depCost:" . urlencode($tmp_extensions);
+					
 				}
+
+				// Centro De Costos
 				if (isset($data['centro']) && !empty($data['centro'])) {
 					$this -> loadModel('SipDispositivo');
 					$extensions = $this -> SipDispositivo -> find('list', array('conditions' => array('SipDispositivo.cost_center_id' => $data['centro']), 'fields' => array('SipDispositivo.name')));
-					$conditions['Cdr.src'] = $extensions;
+					//$conditions['Cdr.src'] = $extensions;
+					$tmp_extensions = "";
+					foreach($extensions as $exten) {
+						$tmp_extensions .= "$exten;";
+					}
+					$redirect .= "/depCost:" . urlencode($tmp_extensions);
 				}
 			}
 			$this -> redirect($redirect);
 		} else {
-			echo 'sin datos';
+			$this->redirect('/cdrs/reporte');
 		}
 	}
 
 	public function reporte() {
-		//$this -> paginate = array('conditions' => $conditions, 'order' => array('Cdr.calldate' => 'ASC'));
+		$queries = null;
+		if(isset($this->params['named']) && !empty($this->params['named'])) { $queries = $this -> params['named']; }
+		$conditions = array();
+		if($queries) {
+			// Fecha, hora inicial
+			if(isset($queries['fi']) && !isset($queries['ff'])) {
+				$conditions['Cdr.calldate >='] = urldecode($queries['fi']);
+			}
+			// Fecha, hora final
+			if(isset($queries['ff']) && !isset($queries['fi'])) {
+				$conditions['Cdr.calldate <='] = urldecode($queries['ff']);
+			}
+			if(isset($queries['fi']) && isset($queries['ff'])) {
+				$conditions['Cdr.calldate BETWEEN ? AND ?'] = array(urldecode($queries['fi']), urldecode($queries['ff']));
+			}
+			// Extensión
+			if(isset($queries['exten']) && !empty($queries['exten'])) {
+				$conditions['OR'] = array(
+					'Cdr.src'=>urldecode($queries['exten']),
+					'Cdr.dst'=>urldecode($queries['exten'])
+				);
+			}
+			// Origen
+			if(isset($queries['src'])) {
+				$conditions['Cdr.src'] = urldecode($queries['src']);
+			}
+			// Destino
+			if(isset($queries['dst'])) {
+				$conditions['Cdr.dst'] = urldecode($queries['dst']);
+			}
+			// Departamentos / Centros De Costos
+			if(isset($queries['depCost'])) {
+				$tmp_extensions = urldecode($queries['depCost']);
+				$tmp_extensions = explode(";", $tmp_extensions);
+				foreach($tmp_extensions as $key=>$val) {
+					if(empty($val)) {
+						unset($tmp_extensions[$key]);
+					}
+				}
+				$conditions['Cdr.src'] = $tmp_extensions;
+			}
+		}
+		$this -> paginate = array('conditions' => $conditions, 'order' => array('Cdr.calldate' => 'ASC'));
 		$cdrs = $this -> paginate();
 		$this -> set('cdrs', $cdrs);
 	}
